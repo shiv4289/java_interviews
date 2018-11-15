be028ff9f334124435d095b535111df0c4aaeb27
shiv <shiv4289@gmail.com>
2018-11-15 10:08:06 +0530

    Added Trie code (Node structure, insert and search APIs)

---
+++ problem_solving/datastructures/trie/TrieNode.java
@@ -0,0 +1,35 @@
+package problem_solving.datastructures.trie;
+
+
+/**
+ * https://leetcode.com/articles/implement-trie-prefix-tree/
+ */
+class TrieNode {
+
+    // R links to node children
+    private TrieNode[] links;
+
+    private final int R = 26;
+
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch -'a'] != null;
+    }
+    public TrieNode get(char ch) {
+        return links[ch -'a'];
+    }
+    public void put(char ch, TrieNode node) {
+        links[ch -'a'] = node;
+    }
+    public void setEnd() {
+        isEnd = true;
+    }
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
